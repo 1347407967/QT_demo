33dd3350b8e7520eabda477292d513c12a41ce0e
JKchen <1347407967@qq.com>
2020-11-13 13:11:38 +0800

    页面

--- app/src/main/java/com/example/qt/MainActivity.java
+++ app/src/main/java/com/example/qt/MainActivity.java
@@ -2,13 +2,17 @@ package com.example.qt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
